04a67121f4ce44aaafbddb1e570d78e28361768c
Shital-P276 <shital.kitcoek@gmail.com>
2024-09-06 03:55:33 -0700

    Create 2-Abstraction_&_Encapsulation.cpp

---
+++ Assignments/2-Abstraction_&_Encapsulation.cpp
@@ -0,0 +1,47 @@
+
+//Math.h-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Math {
+public:
+	int add(int a, int b) {
+		return a + b;
+	}
+
+	int sub(int a, int b) {
+		return a - b;
+	}
+
+	int multiply(int a, int b) {
+		return a * b;
+	}
+
+	int divide(int a, int b) {
+		return a / b;
+	}
+};
+
+//Math.cpp --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#include "Math.h"
+
+int add(int,int);
+int sub(int,int);
+int multiply(int,int);
+int divide(int,int);
+
+//main.cpp---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#include "Math.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	Math obj;
+
+	cout << obj.add(5, 5) << endl;
+	cout << obj.sub(5, 5) << endl;
+	cout << obj.multiply(5, 5) << endl;
+	cout << obj.divide(5, 5) << endl;
+
+	return 0;
+}
