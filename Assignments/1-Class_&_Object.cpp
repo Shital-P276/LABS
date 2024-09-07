7385e39ee4a5c4873d3f8db969018da0902a14b9
Shital-P276 <shital.kitcoek@gmail.com>
2024-09-07 15:27:23 +0530

    Update 1-Class_&_Object.cpp

--- Assignments/1-Class_&_Object.cpp
+++ Assignments/1-Class_&_Object.cpp
@@ -1,1 +1,65 @@
-//do the class and obj assi here
+//    Shital Patil
+//    B77
+//    Assi-1 Class & Objects
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student
+{
+private:
+	// used unsigned int because normal int cant hold PRN like 232400000,etc
+	unsigned int ID;
+	string Name;
+
+public:
+	void setID(unsigned int id)
+	{
+		ID = id;
+	}
+
+	void setName(string name)
+	{
+		Name = name;
+	}
+
+	unsigned int getID()
+	{
+		return ID;
+	}
+
+	string getName()
+	{
+		return Name;
+	}
+};
+
+int main()
+{
+	Student std1;
+	std1.setID(2324001011);
+	std1.setName("Shital Patil");
+
+	cout << "ID: " << std1.getID() << endl;
+	cout << "Name: " << std1.getName() << endl;
+
+	// get from user
+	Student std2;
+	string name;
+	unsigned int id;
+
+	cout << "\n\nEnter Student Name : " << endl;
+	getline(cin, name);
+	std2.setName(name);
+
+	cout << "Enter Student ID : " << endl;
+	cin >> id;
+	std2.setID(id);
+
+	cout << "\nID: " << std2.getID() << endl;
+	cout << "Name: " << std2.getName() << endl;
+
+	return 0;
+}
