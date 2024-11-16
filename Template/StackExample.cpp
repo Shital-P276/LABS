38177c34e02e1859426595d1fe17e1a4e88352e3
Shital-P276 <shital.kitcoek@gmail.com>
2024-11-16 15:47:01 +0530

    Add files via upload

---
+++ Template/StackExample.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+class Node{
+	public:
+		T val;
+		Node(T v){
+			val=v;
+		}
+};
+
+template<class T>
+class Stack{
+	int top;
+	T store[3];
+	public:		
+		class OverFlowException{
+			public: 
+				OverFlowException(){
+					cout<<"Stack Full"<<endl;
+				}
+		};
+		
+		class UnderFlowException{
+			public:
+				UnderFlowException(){
+					cout<<"Stack is empty"<<endl;
+				}
+		};
+		Stack(){
+			top=-1;
+		}
+		void push(T val){
+			try{
+				if(top==2)
+					throw OverFlowException();
+			
+				top++;
+				store[top]=val;
+			}
+			catch(exception n){}
+		}
+		T pop(){
+			if(top==-1) throw UnderFlowException();
+			return store[top--]; 
+		}
+		T peek(){
+			if(top==-1) throw UnderFlowException();
+			return store[top];
+		}
+};
+
+int main(){
+	
+	Stack<int> s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.push(4);
+	cout<<s.peek()<<endl;
+	cout<<s.pop()<<endl;
+	cout<<s.peek()<<endl;
+	cout<<s.pop()<<endl;
+	cout<<s.peek()<<endl;
+	s.pop();
+	s.pop();
+	Stack<char> cs;
+	cs.push('a');
+	cs.push('b');
+	cs.push('c');
+	cout<<cs.peek()<<endl;
+	cout<<cs.pop()<<endl;
+	cout<<cs.peek()<<endl;
+	cout<<cs.pop()<<endl;
+	cout<<cs.peek()<<endl;
+}
